79abc00e87c127d4bfc8efe6be7d5701231eab3c
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-10-07 20:02:14 -0300

    feat: add react icons and implementing icons in form

--- Ecommerce/src/pages/register/register.tsx
+++ Ecommerce/src/pages/register/register.tsx
@@ -1,21 +1,93 @@
-import { RegisterSection } from "./styles";
+import { useState, FormEvent } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+import { RegisterSection, PasswordDiv, ButtonSection } from "./styles";
+
+type viewPasswords = {
+  password: boolean;
+  confirmPassword: boolean;
+};
 
 export function Register() {
+  const [viewPassword, setViewPassword] = useState<viewPasswords>({
+    password: false,
+    confirmPassword: false,
+  });
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newUser = {
+      name: event.currentTarget.userName.value,
+      email: event.currentTarget.email.value,
+      password: event.currentTarget.password.value,
+      passwordConfirmation: event.currentTarget.confirmPassword.value,
+      bithdate: event.currentTarget.birthdate.value,
+      imageURL: event.currentTarget.image.value,
+    };
+    console.log(newUser);
+  }
+
   return (
     <RegisterSection>
-      <form>
-        <label htmlFor="userName"></label>
+      <h2>Cadastrar Usuário</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">Nome de usuário:</label>
         <input type="text" name="userName"></input>
-        <label htmlFor="email"></label>
+        <label htmlFor="email">Email: </label>
         <input type="text" name="email"></input>
-        <label htmlFor="password"></label>
-        <input type="text" name="password"></input>
-        <label htmlFor="confirmPassword"></label>
-        <input type="text" name="confirmPassword"></input>
-        <label htmlFor="bithdate"></label>
+        <label htmlFor="password">Senha:</label>
+        <PasswordDiv>
+          <input
+            type={viewPassword.password ? "text" : "password"}
+            name="password"
+          ></input>
+          <button
+            type="button"
+            onClick={() => {
+              setViewPassword({
+                ...viewPassword,
+                password: !viewPassword.password,
+              });
+            }}
+          >
+            {viewPassword.password ? (
+              <AiFillEyeInvisible size={20} />
+            ) : (
+              <AiFillEye size={20} />
+            )}
+          </button>
+        </PasswordDiv>
+
+        <label htmlFor="confirmPassword">Confirmação de senha</label>
+        <PasswordDiv>
+          <input
+            type={viewPassword.confirmPassword ? "text" : "password"}
+            name="confirmPassword"
+          ></input>
+          <button
+            type="button"
+            onClick={() => {
+              setViewPassword({
+                ...viewPassword,
+                confirmPassword: !viewPassword.confirmPassword,
+              });
+            }}
+          >
+            {viewPassword.confirmPassword ? (
+              <AiFillEyeInvisible size={20} />
+            ) : (
+              <AiFillEye size={20} />
+            )}
+          </button>
+        </PasswordDiv>
+        <label htmlFor="bithdate">Data de nascimento:</label>
         <input type="date" name="birthdate"></input>
-        <label htmlFor="image"></label>
+        <label htmlFor="image">Imagem: </label>
         <input type="text" name="image"></input>
+        <ButtonSection>
+          <button type="submit">Cadastrar</button>
+        </ButtonSection>
       </form>
     </RegisterSection>
   );
--- Ecommerce/src/pages/register/styles.ts
+++ Ecommerce/src/pages/register/styles.ts
@@ -5,13 +5,69 @@ export const RegisterSection = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid whitesmoke;
   width: 50%;
   height: 100%;
 
   form {
     display: flex;
-    width: 80%;
+    min-width: 300px;
     flex-direction: column;
+    margin-top: 15px;
+  }
+
+  input {
+    border-radius: 8px;
+    padding-left: 5px;
+    height: 1.6rem;
+    margin-bottom: 10px;
+  }
+
+  button {
+  }
+`;
+
+export const PasswordDiv = styled.div`
+  display: flex;
+  width: 100%;
+
+  input {
+    display: flex;
+    width: 100%;
+    border: none;
+    border-radius: 8px 0px 0px 8px;
+  }
+
+  button {
+    display: flex;
+    background-color: white;
+    border: none;
+    border-radius: 0px 8px 8px 0px;
+    height: 1.6rem;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    padding-right: 5px;
+  }
+`;
+
+export const ButtonSection = styled.section`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    width: 50%;
+    background-color: white;
+    border: none;
+    border-radius: 5px;
+    height: 1.6rem;
+    cursor: pointer;
+
+    :hover {
+      background-color: rgba(0, 0, 0, 0.4);
+      border: 1px solid white;
+      color: white;
+    }
   }
 `;
